227c31ad2549f2fccc16ab981f169df39c35143b
郭黎辉 <guolihui@guolihuideMacBook-Pro.local>
2019-03-03 18:18:24 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+var usocket = [];
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on("join", function (name) {
+        usocket[name] = socket;
+        io.emit("join", name);
+    });
+
+    socket.on("message", function (msg) {
+       io.emit("message", msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
